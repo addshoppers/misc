007230dcb8f99767ec31e67a5d52757745549aae
Mario Catalano <mario@addshoppers.com>
2018-08-15 09:26:22 -0400

    Update share-trigger.js

--- share-trigger.js
+++ share-trigger.js
@@ -2,4 +2,10 @@
 AddShoppersWidget.API.Event.bind("share", function(params) { 
   //params returns an object {}
   // example params: {"source": "facebook", "event": "share"}
+  
+  //gather user info for the user that just shared
+  var data = AddShoppersWidget.API.User.signed_data(); //this is the user data object returned from the network
+  console.log(data);
+	//DATA another object gets returned with userdata gathered from the network
+	//{google_email: "mario@addshoppers.com", uuid: "1234567890", timestamp: 1534338253, google_firstname: "Mario", signature: "0987654321", timestamp: 12345, uuid: 34543j43j543j543j543j43543 }
 });
